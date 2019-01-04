c7907cf9aa0a680356c019e510d06fa0c923d9bf
LiHuashen <854410664@qq.com>
2019-01-04 15:29:47 +0800

    Update faces_encoder.ino

--- Module/ENCODER/Arduino/faces_encoder/faces_encoder.ino
+++ Module/ENCODER/Arduino/faces_encoder/faces_encoder.ino
@@ -4,29 +4,29 @@
 
 int encoder_increment;//positive: clockwise nagtive: anti-clockwise
 int encoder_value=0;
-int button_state;
-uint8_t direct;//0: clockwise 1: anti-clockwise
+uint8_t direction;//0: clockwise 1: anti-clockwise
+uint8_t last_button, cur_button;
 
 void GetValue(void){
     int temp_encoder_increment;
 
-    Wire.requestFrom(0x5e, 3);
+    Wire.requestFrom(Faces_Encoder_I2C_ADDR, 3);
     if(Wire.available()){
        temp_encoder_increment = Wire.read();
-       button_state = Wire.read();
+       cur_button = Wire.read();
     }
     if(temp_encoder_increment > 127){//anti-clockwise
-        direct = 1;
+        direction = 1;
         encoder_increment = 256 - temp_encoder_increment;
     }
     else{
-        direct = 0;
+        direction = 0;
         encoder_increment = temp_encoder_increment;
     }
 }
 
 void Led(int i, int r, int g, int b){
-    Wire.beginTransmission(0x5e);
+    Wire.beginTransmission(Faces_Encoder_I2C_ADDR);
     Wire.write(i);
     Wire.write(r);
     Wire.write(g);
@@ -58,31 +58,33 @@ void setup()
 void loop()
 {
     int i;
-    M5.Lcd.setCursor(0,40); M5.Lcd.print("Change Value: ");
+    M5.Lcd.setCursor(0,40); M5.Lcd.print("Encoder Value: ");
     M5.Lcd.setCursor(0,60); M5.Lcd.print("  Key State : ");
     M5.Lcd.setCursor(0,80); M5.Lcd.print("  Key Value : ");
     GetValue();
-    if(button_state==0)
-    {
+
+    if(last_button != cur_button){
         M5.Lcd.fillRect(100,60,100,25,BLACK);
         M5.Lcd.fillRect(100,80,100,25,BLACK);
-        M5.Lcd.setCursor(100,60); M5.Lcd.print("pressed");
-        M5.Lcd.setCursor(100,80); M5.Lcd.print("0");
-        for(i=0;i<12;i++)
-        {
-            Led(i, 0xff, 0xff, 0xff);
-        }
+        last_button = cur_button;
     }
-    else{
+    if(cur_button){
         M5.Lcd.setCursor(100,60); M5.Lcd.print("released");
         M5.Lcd.setCursor(100,80); M5.Lcd.print("1");
-        for(i=0;i<12;i++)
-        {
+        for(i=0;i<12;i++){
             Led(i, 0, 0, 0);
         }
     }
+    else{
+        M5.Lcd.setCursor(100,60); M5.Lcd.print("pressed");
+        M5.Lcd.setCursor(100,80); M5.Lcd.print("0");
+        for(i=0;i<12;i++){
+            Led(i, 255, 255, 255);
+        }
+    }
+
     M5.Lcd.fillRect(100,40,50,25,BLACK);
-    if(direct){
+    if(direction){
         encoder_value -= encoder_increment;
         M5.Lcd.setCursor(100,40); M5.Lcd.print("-");
         M5.Lcd.setCursor(100,40); M5.Lcd.print(encoder_value);
@@ -93,5 +95,5 @@ void loop()
         M5.Lcd.setCursor(100,40); M5.Lcd.print(encoder_value);
         Serial.print("encoder_value: "); Serial.print(encoder_value);
     }
-    Serial.print("    button_state: "); Serial.println(button_state);
+    Serial.print("    button_state: "); Serial.println(cur_button);
 }
