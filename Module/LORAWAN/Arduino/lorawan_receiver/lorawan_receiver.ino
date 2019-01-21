5cdf956cc755e68c94dd9e25fb418a8728d63e48
LiHuashen <854410664@qq.com>
2019-01-21 15:34:50 +0800

    updated examples for LORAWAN

--- Module/LORAWAN/Arduino/lorawan_receiver/lorawan_receiver.ino
+++ Module/LORAWAN/Arduino/lorawan_receiver/lorawan_receiver.ino
@@ -1,5 +1,5 @@
 /*
-    Slaver.ino
+    device_B.ino
 */
 
 #include <M5Stack.h>
@@ -10,41 +10,16 @@
 unsigned char buffer[128] = {
     0,
 };
-unsigned char buff[] = {2, 3, 4, 5, 12};
-long lastSendTime = 0; // last send time
-int interval = 300;    // interval between sends
-int send_lastSendTime = 0;
-int send_interval = 300; // interval between sends
 uint8_t flag_test_868 = 0;
 
 void key_scan(void *arg)
 {
-    while (1)
-    {
-            if (M5.BtnA.wasPressed())
-    {
-        M5.Lcd.setCursor(0, 20);
-        M5.Lcd.fillRect(0, 20, 320, 215, BLACK);
-    }
-    if (M5.BtnB.wasPressed())
-    {
-        flag_test_868 = 0;
-        init_433();
-        send_data();
-        delay(300);
-        M5.Lcd.println("btn B  Test frequency 433");
-    }
-    if (M5.BtnC.wasPressed())
-    {
-        flag_test_868 = 1;
-        init_868();
-        send_data();
-        delay(300);
-        M5.Lcd.println("btn C  Test frequency 868");
-    }
-    M5.update();
-    }
-    vTaskDelete(NULL);
+  while (1)
+  {
+    onReceive();
+    // lora.loraDebug();
+  }
+  vTaskDelete(NULL);
 }
 
 void setup(void)
@@ -52,53 +27,45 @@ void setup(void)
   M5.begin();
   SerialUSB.begin(9600);
   lora.init();
-  delay(2000); // delay for lorawan power on
+  delay(2000); // must delay for lorawan power on
   lora.initP2PMode(433, SF12, BW500, 8, 8, 20);
   M5.Lcd.setTextFont(2);
-  M5.Lcd.println("receiver");
-  M5.Lcd.setTextColor(YELLOW);
-      int core = xPortGetCoreID();
-    M5.Lcd.println(core);
+  M5.Lcd.println("Device B");
+  M5.Lcd.setTextColor(BLUE);
+  int core = xPortGetCoreID();
+  // M5.Lcd.println(core);
   xTaskCreatePinnedToCore(key_scan, "key_scan", 2048, NULL, 0, NULL, 0);
 }
 
 void loop(void)
 {
   // lora.loraDebug();
-
-  if (millis() - lastSendTime > interval)
+  if (M5.BtnA.wasPressed())
   {
-    Serial2.print("AT+TEST=RXLRPKT\r\n");
-    delay(50);
-    lora.loraDebug();
-    // onReceive();
-    lastSendTime = millis();
-    interval = random(300) + interval;
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.fillRect(0, 20, 320, 215, BLACK);
   }
-  // if (millis() - send_lastSendTime > send_interval)
-  // {
-  //   send_data();
-  //   send_lastSendTime = millis();
-  //   send_interval = random(50) + send_interval;
-  // }
-  // if (M5.BtnA.wasPressed())
-  // {
-  //   M5.Lcd.setCursor(0, 20);
-  //   M5.Lcd.fillRect(0, 20, 320, 215, BLACK);
-  // }
-  // if (M5.BtnB.wasPressed())
-  // {
-  //   flag_test_868 = 0;
-  //   init_433();
-  //   Serial.println("btn B  Test frequency 433");
-  // }
-  // if (M5.BtnC.wasPressed())
-  // {
-  //   flag_test_868 = 1;
-  //   init_868();
-  //   Serial.println("btn C  Test frequency 868");
-  // }
-  // M5.update();
+  if (M5.BtnB.wasPressed()) //send data (433MHz)
+  {
+    flag_test_868 = 0;
+    M5.Lcd.setTextColor(BLUE);
+    init_433();
+    send_data();
+    delay(300);
+    Serial2.print("AT+TEST=RXLRPKT\r\n");
+        M5.Lcd.println("433M Init and Send \"Hello World\"...");
+  }
+  if (M5.BtnC.wasPressed()) //send data (868MHz)
+  {
+    flag_test_868 = 1;
+    M5.Lcd.setTextColor(YELLOW);
+    init_868();
+    send_data();
+    delay(300);
+    Serial2.print("AT+TEST=RXLRPKT\r\n");
+    M5.Lcd.println("868M Init and Send \"Hello World\"...");
+  }
+  M5.update();
 }
 
 void init_433()
@@ -114,7 +81,6 @@ void init_868()
 void send_data()
 {
   lora.transferPacketP2PMode("hello world");
-  //   Serial2.print("AT+TEST=RXLRPKT\r\n");
 }
 
 void onReceive()
@@ -134,19 +100,19 @@ void onReceive()
     SerialUSB.print("Data is: ");
     if (flag_test_868)
     {
-      M5.Lcd.print("Test Freq 868:  ");
+      M5.Lcd.print("868M Recv:  ");
     }
     else
     {
-      M5.Lcd.print("Test Freq 433:  ");
+      M5.Lcd.print("433M Recv:  ");
     }
     for (unsigned char i = 0; i < length; i++)
     {
       SerialUSB.print((char)buffer[i]);
       M5.Lcd.print((char)buffer[i]);
-      // SerialUSB.print("0x");
-      // SerialUSB.print(buffer[i], HEX);
-      // SerialUSB.print(" ");
+      //   SerialUSB.print("0x");
+      //   SerialUSB.print(buffer[i], HEX);
+      //   SerialUSB.print(" ");
     }
     SerialUSB.println();
     M5.Lcd.println();
--- Module/LORAWAN/Arduino/lorawan_transmmiter/lorawan_transmmiter.ino
+++ Module/LORAWAN/Arduino/lorawan_transmmiter/lorawan_transmmiter.ino
@@ -1,5 +1,5 @@
 /*
-    Master.ino
+    device_A.ino
 */
 #include <M5Stack.h>
 #include <LoRaWan.h>
@@ -9,39 +9,14 @@
 unsigned char buffer[128] = {
     0,
 };
-unsigned char buff[] = {2, 3, 4, 5, 12};
-long lastSendTime = 0; // last send time
-int interval = 300;    // interval between sends
-int send_lastSendTime = 0;
-int send_interval = 300; // interval between sends
 uint8_t flag_test_868 = 0;
 
 void key_scan(void *arg)
 {
     while (1)
     {
-    if (M5.BtnA.wasPressed())
-    {
-        M5.Lcd.setCursor(0, 20);
-        M5.Lcd.fillRect(0, 20, 320, 215, BLACK);
-    }
-    if (M5.BtnB.wasPressed())
-    {
-        flag_test_868 = 0;
-        init_433();
-        send_data();
-        delay(300);
-        M5.Lcd.println("btn B  Test frequency 433");
-    }
-    if (M5.BtnC.wasPressed())
-    {
-        flag_test_868 = 1;
-        init_868();
-        send_data();
-        delay(300);
-        M5.Lcd.println("btn C  Test frequency 868");
-    }
-    M5.update();
+        onReceive();
+        // lora.loraDebug();
     }
     vTaskDelete(NULL);
 }
@@ -51,57 +26,44 @@ void setup(void)
     M5.begin();
     SerialUSB.begin(9600);
     lora.init();
-    delay(2000); // elay for lorawan power on
+    delay(2000); // must delay for lorawan power on
     lora.initP2PMode(433, SF12, BW500, 8, 8, 20);
     M5.Lcd.setTextFont(2);
-    M5.Lcd.println("transmitter");
+    M5.Lcd.println("Device A");
     M5.Lcd.setTextColor(BLUE);
     int core = xPortGetCoreID();
-    M5.Lcd.println(core);
+    // M5.Lcd.println(core);
     xTaskCreatePinnedToCore(key_scan, "key_scan", 2048, NULL, 0, NULL, 0);
 }
 
 void loop(void)
 {
-    //   lora.loraDebug();
-    if (millis() - lastSendTime > interval)
+    if (M5.BtnA.wasPressed())
     {
-        Serial2.print("AT+TEST=RXLRPKT\r\n");
-        delay(50);
-        lora.loraDebug();
-        // onReceive();
-        lastSendTime = millis();
-        interval = random(300) + interval;
+        M5.Lcd.setCursor(0, 20);
+        M5.Lcd.fillRect(0, 20, 320, 215, BLACK);
     }
-    // if (millis() - send_lastSendTime > send_interval)
-    // {
-    //     send_data();
-    //     send_lastSendTime = millis();
-    //     send_interval = random(50) + send_interval;
-    // }
-    // if(flag_test_868){
-    //     flag_test_868 = 0;
-    //     init_868();
-    //     M5.Lcd.println("btn C  Test frequency 868");
-    // }
-    // if (M5.BtnA.wasPressed())
-    // {
-    //     M5.Lcd.setCursor(0, 20);
-    //     M5.Lcd.fillRect(0, 20, 320, 215, BLACK);
-    // }
-    // if (M5.BtnB.wasPressed())
-    // {
-    //     flag_test_868 = 0;
-    //     init_433();
-    //     Serial.println("btn B  Test frequency 433");
-    // }
-    // if (M5.BtnC.wasPressed())
-    // {
-    //     flag_test_868 = 1;
-    //     init_868();
-    //     Serial.println("btn C  Test frequency 868");
-    // }
-    // M5.update();
+    if (M5.BtnB.wasPressed()) //send data (433MHz)
+    {
+        flag_test_868 = 0;
+        M5.Lcd.setTextColor(BLUE);
+        init_433();
+        send_data();
+        delay(300);
+        Serial2.print("AT+TEST=RXLRPKT\r\n");
+        M5.Lcd.println("433M Init and Send \"Hello World\"...");
+    }
+    if (M5.BtnC.wasPressed()) //send data (868MHz)
+    {
+        flag_test_868 = 1;
+        M5.Lcd.setTextColor(YELLOW);
+        init_868();
+        send_data();
+        delay(300);
+        Serial2.print("AT+TEST=RXLRPKT\r\n");
+        M5.Lcd.println("868M Init and Send \"Hello World\"...");
+    }
+    M5.update();
 }
 
 void init_433()
@@ -117,7 +79,6 @@ void init_868()
 void send_data()
 {
     lora.transferPacketP2PMode("hello world");
-    //   Serial2.print("AT+TEST=RXLRPKT\r\n");
 }
 
 void onReceive()
@@ -137,11 +98,11 @@ void onReceive()
         SerialUSB.print("Data is: ");
         if (flag_test_868)
         {
-            M5.Lcd.print("Test Freq 868:  ");
+            M5.Lcd.print("868M Recv:  ");
         }
         else
         {
-            M5.Lcd.print("Test Freq 433:  ");
+            M5.Lcd.print("433M Recv:  ");
         }
         for (unsigned char i = 0; i < length; i++)
         {
