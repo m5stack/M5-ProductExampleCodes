8ddfbdeb3a849447949b63127b87b0f6ac471443
LiHuashen <854410664@qq.com>
2019-02-19 18:03:09 +0800

    Update trace.ino

--- Unit/TRACE/Arduino/trace.ino
+++ Unit/TRACE/Arduino/trace.ino
@@ -3,7 +3,7 @@
 // #define VALUE_SPLIT
 
 uint8_t value;
-int sensorValue[4] = {0};
+int SensorArray[4] = {0};
 
 void setup() {
     m5.begin();
@@ -32,15 +32,15 @@ void SensorStatus(void){
     }
     Serial.print(" value = ");Serial.println(value, HEX);
 #ifdef VALUE_SPLIT
-    sensorValue[0] = (value&0x08)>>3;
-    sensorValue[1] = (value&0x04)>>2;
-    sensorValue[2] = (value&0x02)>>1;
-    sensorValue[3] = (value&0x01)>>0;
+    SensorArray[3] = (value&0x08)>>3;
+    SensorArray[2] = (value&0x04)>>2;
+    SensorArray[1] = (value&0x02)>>1;
+    SensorArray[0] = (value&0x01)>>0;
     Serial.println("After splitting... ");
-    Serial.print("SensorValue[0] = ");Serial.print(sensorValue[0]);
-    Serial.print("  SensorValue[1] = ");Serial.print(sensorValue[1]);
-    Serial.print("  SensorValue[2] = ");Serial.print(sensorValue[2]);
-    Serial.print("  SensorValue[3] = ");Serial.print(sensorValue[3]);
+    Serial.print("SensorArray[0] = ");Serial.print(SensorArray[0]);
+    Serial.print("  SensorArray[1] = ");Serial.print(SensorArray[1]);
+    Serial.print("  SensorArray[2] = ");Serial.print(SensorArray[2]);
+    Serial.print("  SensorArray[3] = ");Serial.print(SensorArray[3]);
     Serial.println();
 #endif
 }
