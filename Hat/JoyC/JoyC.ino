de24022b88b9ecbfe8c802311fc71517cea8436e
vany5921 <908744431@qq.com>
2019-11-11 19:26:36 +0800

    change RoverC demo

--- Hat/JoyC/JoyC.ino
+++ Hat/JoyC/JoyC.ino
@@ -1,6 +1,9 @@
 #include <M5StickC.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include "EEPROM.h"
+
+#define EEPROM_SIZE 64
 
 TFT_eSprite Disbuff = TFT_eSprite(&M5.Lcd);
 extern const unsigned char connect_on[800];
@@ -18,7 +21,7 @@ IPAddress subnet(255, 255, 0, 0);
 IPAddress primaryDNS(8, 8, 8, 8); //optional
 IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
-const char *ssid = "HadesAP2468";
+const char *ssid = "M5AP";
 const char *password = "77777777";
 
 WiFiUDP Udp;  
@@ -90,12 +93,15 @@ void SendUDP()
 	}
 }
 
+char APName[20];
+String WfifAPBuff[16];
+uint32_t count_bn_a = 0, choose = 0;
+String ssidname;
 void setup() {
   // put your setup code here, to run once:
     M5.begin();
-    M5.update();
     Wire.begin(0,26,10000);
-
+	EEPROM.begin(EEPROM_SIZE);
 
 	M5.Lcd.setRotation(4);
     M5.Lcd.setSwapBytes(false);
@@ -103,32 +109,143 @@ void setup() {
     Disbuff.setSwapBytes(true);
 
 	Disbuff.fillRect(0,0,80,20,Disbuff.color565(50,50,50));
-	Disbuff.setTextSize(1);
-	Disbuff.setTextColor(WHITE);
+	Disbuff.setTextSize(2);
+	Disbuff.setTextColor(GREEN);
 	Disbuff.setCursor(55,6);
-	Disbuff.printf("%03d",SYSNUM);
+	//Disbuff.printf("%03d",SYSNUM);
 
 	Disbuff.pushImage(0,0,20,20,(uint16_t *)connect_off);
 	Disbuff.pushSprite(0,0);
 
-	//I2CWrite1Byte(0x10,0x02);
-
+	uint8_t res = I2CRead8bit(0x32);
+	Serial.printf("Res0 = %02X \r\n",res );
 	/*
-	
+	if( res & 0x02 )
+	{
+		I2CWrite1Byte(0x10,0x02);
+	}
+	else
+	{
+		Disbuff.setTextSize(1);
+		Disbuff.setTextColor(GREEN);
+		Disbuff.fillRect(0,0,80,20,Disbuff.color565(50,50,50));
+		Disbuff.setCursor(5,20);
+		Disbuff.printf("calibration");
+		Disbuff.pushSprite(0,0);
+		I2CWrite1Byte( 0x32 , 0x01 );
+	}
+	*/
+	M5.update();
+	if(( EEPROM.read(0) != 0x56 )||( M5.BtnA.read() == 1 ))
+	{
+		WiFi.mode(WIFI_STA);
+		int n = WiFi.scanNetworks();
+		Disbuff.setTextSize(1);
+		Disbuff.setTextColor(GREEN);
+		Disbuff.fillRect(0,0,80,20,Disbuff.color565(50,50,50));
+		
+		if (n == 0) 
+		{
+			Disbuff.setCursor(5,20);
+			Disbuff.printf("no networks");
+
+		} 
+		else {
+			int count = 0;
+			for (int i = 0; i < n; ++i) {
+				if( WiFi.SSID(i).indexOf("M5AP") != -1 )
+				{
+					if( count == 0 )
+					{
+						Disbuff.setTextColor(GREEN);
+					}
+					else
+					{
+						Disbuff.setTextColor(WHITE);
+					}
+					Disbuff.setCursor(5,25 + count *10 );
+					String str = WiFi.SSID(i);
+					Disbuff.printf(str.c_str());
+					WfifAPBuff[count] = WiFi.SSID(i);
+					count++;
+				}
+			}
+			Disbuff.pushSprite(0,0);
+			while( 1 )
+			{
+				if( M5.BtnA.read() == 1 )
+				{	
+					if( count_bn_a >= 200 )
+					{
+						count_bn_a = 201;
+						EEPROM.writeUChar(0,0x56);
+						EEPROM.writeString(1,WfifAPBuff[choose]);
+						ssidname = WfifAPBuff[choose];
+						break;
+					}
+					count_bn_a ++;
+					Serial.printf("count_bn_a %d \n", count_bn_a );
+				}
+				else if(( M5.BtnA.isReleased())&&( count_bn_a != 0 ))
+				{
+					Serial.printf("count_bn_a %d",count_bn_a);
+					if( count_bn_a > 200 )
+					{
+						
+					}
+					else
+					{
+						choose ++;
+						if( choose >= count )
+						{
+							choose = 0;
+						}
+						Disbuff.fillRect(0,0,80,20,Disbuff.color565(50,50,50));
+						for (int i = 0; i < count; i++)
+						{
+							Disbuff.setCursor(5,25 + i *10 );
+							if( choose == i )
+							{
+								Disbuff.setTextColor(GREEN);
+							}
+							else
+							{
+								Disbuff.setTextColor(WHITE);
+							}
+							Disbuff.printf(WfifAPBuff[i].c_str());
+						}
+						Disbuff.pushSprite(0,0);
+					}
+					count_bn_a = 0;
+				}
+				delay(10);
+				M5.update();
+			}
+			//EEPROM.writeString(1,WfifAPBuff[0]);
+    	}
+	}
+	else if( EEPROM.read(0) == 0x56 )
+	{
+		ssidname = EEPROM.readString(1);
+		EEPROM.readString(1,APName,16);
+	}
+
+	Disbuff.fillRect(0,20,80,140,BLACK);
+
 	if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) 
 	{
     	Serial.println("STA Failed to configure");
   	}
 
-	WiFi.begin(ssid, password);
+	WiFi.begin(ssidname.c_str(), password);
 
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(500);
 		Serial.print(".");
   	}
+	
 
 	Udp.begin(2000);
-	*/
 	
 	Disbuff.pushImage(0,0,20,20,(uint16_t *)connect_on);
 	Disbuff.pushSprite(0,0);
@@ -139,6 +256,7 @@ uint8_t adc_value[5] = {0};
 uint16_t AngleBuff[4];
 uint32_t count = 0;
 void loop() {
+
 
 	for (int i = 0; i < 5; i++)
 	{
@@ -151,7 +269,7 @@ void loop() {
 	}
 
 	delay(10);
-	/*
+
 	
 	if( WiFi.status() != WL_CONNECTED )
 	{
@@ -167,14 +285,18 @@ void loop() {
 	}
 	else
 	{
+		SendBuff[3] = map(AngleBuff[0],0,4000,0,200);
+		SendBuff[4] = map(AngleBuff[1],0,4000,0,200);
+		SendBuff[5] = map(AngleBuff[2],0,4000,0,200);
+		/*
 		Disbuff.pushImage(0,0,20,20,(uint16_t *)connect_on);
 		Disbuff.pushSprite(0,0);
 		count = 0;
-		if(( AngleBuff[2] != 0 )||( AngleBuff[3] != 0 ))
+		*/
+		if( ( SendBuff[3] > 110  )||( SendBuff[3] < 90 )||\
+			( SendBuff[4] > 110  )||( SendBuff[4] < 90 )||\
+			( SendBuff[5] > 110  )||( SendBuff[5] < 90 ))
 		{
-			SendBuff[3] = adc_value[0];
-			SendBuff[4] = adc_value[1];
-			SendBuff[5] = adc_value[2];
 			SendBuff[6] = 0x01;
 		}
 		else
@@ -183,18 +305,17 @@ void loop() {
 		}
 		SendUDP();
 	}
-	*/
+	
 
 	Disbuff.fillRect( 0,30,80,130,BLACK);
 	Disbuff.setCursor(10,30);
-	Disbuff.printf("%04d",AngleBuff[0]);
-	Disbuff.setCursor(10,40);
-	Disbuff.printf("%04d",AngleBuff[1]);
-	Disbuff.setCursor(10,50);
-	Disbuff.printf("%04d",AngleBuff[2]);
+	Disbuff.printf("%04d",SendBuff[3]);
+	Disbuff.setCursor(10,45);
+	Disbuff.printf("%04d",SendBuff[4]);
 	Disbuff.setCursor(10,60);
-	Disbuff.printf("%04d",AngleBuff[3]);
-	Disbuff.setCursor(10,70);
+	Disbuff.printf("%04d",SendBuff[5]);
+
+	Disbuff.setCursor(10,100);
 	Disbuff.printf("%04X",adc_value[4]);
 	Disbuff.pushSprite(0,0);
 }
--- Hat/RoverC/RoverC.ino
+++ Hat/RoverC/RoverC.ino
@@ -2,13 +2,21 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-const char *ssid = "HadesAP";
+const char *ssid = "M5AP";
 const char *password = "77777777";
 
 TFT_eSprite Disbuff = TFT_eSprite(&M5.Lcd);
 WiFiServer server(80);
 
 WiFiUDP Udp1;
+
+void SetChargingCurrent( uint8_t CurrentLevel )
+{
+	Wire1.beginTransmission(0x34);
+	Wire1.write(0x33);
+	Wire1.write(0xC0 | ( CurrentLevel & 0x0f));
+	Wire1.endTransmission();
+}
 
 void setup()
 {
@@ -18,16 +26,18 @@ void setup()
 
 	uint64_t chipid=ESP.getEfuseMac();
 	String str = ssid + String((uint32_t)(chipid>>32),HEX);
-	M5.Lcd.setRotation(4);
+	M5.Lcd.setRotation(1);
 	M5.Lcd.setSwapBytes(false);
-	Disbuff.createSprite(80, 160);
+	Disbuff.createSprite(160, 80);
 	Disbuff.setSwapBytes(true);
-	Disbuff.fillRect(0, 0, 80, 20, Disbuff.color565(50, 50, 50));
-	Disbuff.setTextSize(1);
+	Disbuff.fillRect(0, 0, 160, 20, Disbuff.color565(50, 50, 50));
+	Disbuff.setTextSize(2);
 	Disbuff.setTextColor(WHITE);
-	Disbuff.setCursor(0, 6);
+	Disbuff.setCursor(15, 35);
 	Disbuff.print(str);
 	Disbuff.pushSprite(0, 0);
+
+	SetChargingCurrent(6);
 
 	Serial.begin(115200);
 	//Set device in STA mode to begin with
@@ -53,16 +63,15 @@ uint8_t SendBuff[9] = {0xAA, 0x55,
 					   0x00,
 					   0xee};
 
-
-void I2CWrite1Byte( uint8_t Addr ,  uint8_t Data )
+uint8_t I2CWrite1Byte( uint8_t Addr ,  uint8_t Data )
 {
 	Wire.beginTransmission(0x38);
 	Wire.write(Addr);
 	Wire.write(Data);
-	Wire.endTransmission();
+	return Wire.endTransmission();
 }
 
-void I2CWritebuff( uint8_t Addr,  uint8_t* Data, uint16_t Length )
+uint8_t I2CWritebuff( uint8_t Addr,  uint8_t* Data, uint16_t Length )
 {
 	Wire.beginTransmission(0x38);
 	Wire.write(Addr);
@@ -70,14 +79,16 @@ void I2CWritebuff( uint8_t Addr,  uint8_t* Data, uint16_t Length )
 	{
 		Wire.write(Data[i]);
 	}
-	Wire.endTransmission();
+	return Wire.endTransmission();
 }
+
 
 int16_t speed_buff[4] = {0};
 int8_t  speed_sendbuff[4] = {0};
 uint32_t count = 0;
+uint8_t IIC_ReState = I2C_ERROR_NO_BEGIN;
 
-void Setspeed( int16_t Vtx, int16_t Vty, int16_t Wt)
+uint8_t Setspeed( int16_t Vtx, int16_t Vty, int16_t Wt)
 {
 	Wt = ( Wt > 100 )  ? 100 :  Wt;
 	Wt = ( Wt < -100 ) ? -100 : Wt;
@@ -101,6 +112,7 @@ void Setspeed( int16_t Vtx, int16_t Vty, int16_t Wt)
 		speed_buff[i] = ( speed_buff[i] < -100 ) ? -100 : speed_buff[i];
 		speed_sendbuff[i] = speed_buff[i];
 	}
+	return I2CWritebuff(0x00,(uint8_t*)speed_sendbuff,4);
 }
 
 void loop()
@@ -120,14 +132,29 @@ void loop()
 			Serial.println();
 			if( udodata[6] == 0x01 )
 			{
-				Setspeed( udodata[3] - 100  ,udodata[4] - 100 , udodata[5] -100 );
+				IIC_ReState = Setspeed( udodata[3] - 100  ,udodata[4] - 100 , udodata[5] -100 );
 			}
 			else
 			{
-				Setspeed( 0  ,0 , 0 );
+				IIC_ReState = Setspeed( 0  ,0 , 0 );
 			}
-			
 		}
+		else
+		{
+			IIC_ReState = Setspeed( 0  ,0 , 0 );
+		}
+		
 	}
-	count ++;   
+	count ++; 
+	if( count > 100 )
+	{
+		count = 0;
+
+		Disbuff.fillRect(0, 0, 160, 20, Disbuff.color565(50, 50, 50));
+		Disbuff.setTextSize(1);
+		Disbuff.setTextColor(WHITE);
+		Disbuff.setCursor(5, 5);
+		Disbuff.printf("%.2fV,%.2fmA,%d",M5.Axp.GetBatVoltage(),M5.Axp.GetBatCurrent(),IIC_ReState);
+		Disbuff.pushSprite(0, 0);
+	}  
 }
