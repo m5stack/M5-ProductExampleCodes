94e85a53d499dcf8a038c99ba81d9b7616501138
vany5921 <908744431@qq.com>
2020-06-05 15:16:36 +0800

    add servo kit example

---
+++ Accessory/ServoKit180_360/ServoKit180_360.ino
@@ -0,0 +1,26 @@
+//Control rotation angle  or speed and direction by sending pulse 
+//Servo PIN 26
+
+#define COUNT_LOW 1500
+#define COUNT_HIGH 8500
+#define TIMER_WIDTH 16
+#include "esp32-hal-ledc.h"
+#include <M5Stack.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin();
+  ledcSetup(1, 50, TIMER_WIDTH);
+  ledcAttachPin(26, 1);
+  M5.Lcd.setCursor(20, 80, 2);
+  M5.Lcd.print("SERVO");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = COUNT_LOW; i < COUNT_HIGH; i = i + 100){  
+    ledcWrite(1, i);
+    delay(50);
+  }
+}
